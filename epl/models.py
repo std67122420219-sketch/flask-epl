e6b6fe719d7a5641741fbca723aa83675e1d7e25
std67122420219-sketch <std.67122420219@ubru.ac.th>
2026-02-03 12:25:05 +0700

    commit

--- epl/models.py
+++ epl/models.py
@@ -4,28 +4,30 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 
 class Club(db.Model):
-  __tablename__ = 'club'
-  id: Mapped[int] = mapped_column(Integer, primary_key=True)
-  name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-  stadium: Mapped[str] = mapped_column(String(50), nullable=False)
-  year: Mapped[int] = mapped_column(Integer, nullable=False)
-  logo: Mapped[str] = mapped_column(String(255), nullable=False)
+    __tablename__ = 'club'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    stadium: Mapped[str] = mapped_column(String(50), nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    logo: Mapped[str] = mapped_column(String(255), nullable=False)
 
-  players: Mapped[List['Player']] = relationship(back_populates='club')
-
-  def __repr__(self):
-    return f'<Club: {self.name}>'
-  
+    players: Mapped[List['Player']] = relationship(back_populates='club')
+    
+    def __repr__(self):
+        return f'<Club: {self.name}>'
+    
 class Player(db.Model):
-  __tablename__ = 'player'
-  id: Mapped[int] = mapped_column(Integer, primary_key=True)
-  name: Mapped[str] = mapped_column(String(50), nullable=False)
-  position: Mapped[str] = mapped_column(String(25), nullable=False)
-  nationality: Mapped[str] = mapped_column(String(50), nullable=False)
-  img: Mapped[str] = mapped_column(String(255), nullable=False)
-  club_id: Mapped[int] = mapped_column(Integer, ForeignKey(Club.id))
+    __tablename__ = 'player'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    position: Mapped[str] = mapped_column(String(20), nullable=False)
+    nationality: Mapped[str] = mapped_column(String(50), nullable=False)
+    goals: Mapped[int] = mapped_column(Integer, default=0, nullable=True)
+    squad_no: Mapped[int] = mapped_column(Integer, nullable=True)
+    img: Mapped[str] = mapped_column(String(255), nullable=False)
+    club_id: Mapped[int] = mapped_column(Integer, ForeignKey(Club.id))
 
-  club: Mapped['Club'] = relationship(back_populates='players')
-
-  def __repr__(self):
-    return f'<Club: {self.name}>'
+    club: Mapped[Club] = relationship(back_populates='players')
+    
+    def __repr__(self):
+        return f'<Club: {self.name}>'
