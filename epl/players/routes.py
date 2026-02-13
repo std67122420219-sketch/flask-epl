6f93e835ad891d5d32e92cb92f3167632984f0a2
std67122420219-sketch <std.67122420219@ubru.ac.th>
2026-02-13 22:00:52 +0700

    งานแก้ commit

--- epl/players/routes.py
+++ epl/players/routes.py
@@ -20,26 +20,23 @@ def new_player():
     position = request.form['position']
     nationality = request.form['nationality']
     goal = int(request.form['goals'])
+    clean_sheets_val = request.form.get('clean_sheets')
+    clean_sheets = None
+    if position == 'Goalkeeper' and clean_sheets_val:
+         clean_sheets = int(clean_sheets_val)
     squad_no = int(request.form['squad_no'])
     img = request.form['img']
     club_id = int(request.form['club_id'])
 
-    if position == 'Goalkeeper':
-        goal = 0
-        clean_sheets = int(request.form['clean_sheets'])
-    else:
-        goal = int(request.form['goals'])
-        clean_sheets = None
-
     player = Player(name=name, position=position, nationality=nationality,
                     goal=goal, clean_sheets=clean_sheets, squad_no=squad_no, img=img, club_id=club_id)
-    
+
     db.session.add(player)
     db.session.commit()
 
     flash('add new player successfully', 'success')
     return redirect(url_for('players.index'))
-  
+
   return render_template('players/new_player.html',
                          title='New Player Page',
                          clubs=clubs)
@@ -52,7 +49,7 @@ def search_player():
     return render_template('players/search_player.html',
                            title='Search Player Page',
                            players=players)
-  
+
 @players_bp.route('/<int:id>/info')
 def info_player(id):
   player = db.session.get(Player, id)
@@ -67,38 +64,31 @@ def update_player(id):
   if request.method == 'POST':
     name = request.form['name']
     position = request.form['position']
+    nationality = request.form['nationality']
+    goal = int(request.form['goals'])
     clean_sheets_val = request.form.get('clean_sheets')
     if position == 'Goalkeeper' and clean_sheets_val:
         player.clean_sheets = int(clean_sheets_val)
     else:
         player.clean_sheets = None
-    nationality = request.form['nationality']
-    goal = int(request.form['goals'])
     squad_no = int(request.form['squad_no'])
     img = request.form['img']
     club_id = int(request.form['club_id'])
 
     player.name = request.form['name']
-    position = request.form['position']
     player.position = position
-    player.nationality = request.form['nationality']
-    player.squad_no = int(request.form['squad_no'])
-    player.img = request.form['img']
-    player.club_id = int(request.form['club_id'])
+    player.nationality = nationality
+    player.goal = goal
+    player.squad_no = squad_no
+    player.img = img
+    player.club_id = club_id
 
-    if position == 'Goalkeeper':
-        player.goal = 0
-        player.clean_sheets = int(request.form['clean_sheets'])
-    else:
-        player.goal = int(request.form['goals'])
-        player.clean_sheets = None
-    
     db.session.add(player)
     db.session.commit()
 
     flash('update player successfully', 'success')
     return redirect(url_for('players.index'))
-  
+
   return render_template('players/update_player.html',
                          title='Update Player Page',
                          player=player,
